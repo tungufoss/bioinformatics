1d961ff50c56e222df298ab1730b8bd384a621de
Helga Ingimundardóttir <tungufoss@gmail.com>
2016-12-14 15:27:31 +0000

    Solve the De Bruijn Graph from a String Problem - WIP
    Code Challenge: Solve the De Bruijn Graph from a String Problem.
    Input: An integer k and a string Text.
    Output: DeBruijnk(Text), in the form of an adjacency list.
    
    Extra Dataset
    
    Visit the code-graded problem
    
    Sample Input:
    4
    AAGATTCTCTAAGA
    Sample Output:
    AAG -> AGA,AGA
    AGA -> GAT
    ATT -> TTC
    CTA -> TAA
    CTC -> TCT
    GAT -> ATT
    TAA -> AAG
    TCT -> CTA,CTC
    TTC -> TCT
    
    FYI: Submits wrong answer for dataset_199_6.out

--- 2-genome-sequencing/week1/graph.py
+++ 2-genome-sequencing/week1/graph.py
@@ -9,9 +9,13 @@ def ReadKmers(inputFile):
   return k,kmers
 
 def Composition(k,txt):
+  printout = len(txt)<25
   kmers = []
   for i in range(len(txt)-k+1):
-    kmers.append(txt[i:(i+k)])
+    kmer = txt[i:(i+k)]
+    if printout : print '{}{}'.format(' '*i,kmer)
+    kmers.append(kmer)
+  if printout : print txt
   return kmers
 
 def StringSpelledByGenomePath(kmers):
@@ -20,27 +24,52 @@ def StringSpelledByGenomePath(kmers):
     txt += kmers[i][-1]
   return txt
 
+
+def Fixes(kmers):
+  prefix = []
+  suffix = []
+  for kmer in kmers:
+    prefix.append(kmer[:-1])
+    suffix.append(kmer[1:])
+  return prefix,suffix
+
 def Overlap(kmers):
+  dups_kmers = list(set([x for x in kmers if kmers.count(x) > 1]))  
+  kmers = reduce(lambda l, x: l.append(x) or l if x not in l else l, kmers, []) # unique kmers
+  prefix,suffix = Fixes(kmers)
   adj = {}
-  nNodes = len(kmers)
-  for i in range(nNodes):
-    for j in range(nNodes):
+  n = len(prefix)
+  for i in range(n):
+    for j in range(n):
       if i == j : continue
-      suffix = kmers[i][1:]
-      prefix = kmers[j][:-1]
-      if suffix != prefix : continue
+      if suffix[i] != prefix[j] : continue
       if i not in adj : 
         adj[i] = [j]
-      else : 
+      else :
         adj[i].append(j)
-        
+
+  # add duplicate matching edges
+  if len(dups_kmers) > 0:
+    n = len(dups_kmers)
+    for ii in range(n):
+      i = kmers.index(dups_kmers[ii])
+      for jj in range(n):
+        if ii==jj : continue
+        j = kmers.index(dups_kmers[jj])
+        if suffix[i] != prefix[j] : continue        
+        if i not in adj : 
+          adj[i] = [j]
+        else :
+          adj[i].append(j)
+
   return adj 
 
 def AdjencyListToString(adj,kmers):
   txt = []
   for i in adj:
-    for j in adj[i]:
-      txt.append('{} -> {}'.format(kmers[i],kmers[j]))
+    from_node = kmers[i]
+    to_nodes = ','.join([kmers[j] for j in adj[i]])
+    txt.append('{} -> {}'.format(from_node,to_nodes))
   return txt
 
 def main_composition(myfile):
@@ -70,11 +99,26 @@ def main_overlap(myfile):
   with open(outputFile,'w') as outFile:
     outFile.write('\n'.join(sorted(txt)))
 
+def main_debruijn_fromstring(myfile):
+  inputFile = myfile + '.txt'
+  outputFile = myfile + '.out'
+  with open(inputFile,'r') as inFile:
+    k = int(inFile.readline().strip())
+    txt = inFile.readline().strip()
+  kmers = Composition(k-1,txt)
+  adj = Overlap(kmers)
+  txt = AdjencyListToString(adj,kmers) 
+  with open(outputFile,'w') as outFile:
+    txt='\n'.join(sorted(txt))
+    outFile.write(txt)
+
 '''
 main_composition('sample_composition')
 main_composition('dataset_197_3')
 main_genomepath('sample_genomepath')
 main_genomepath('dataset_198_3')
-'''
 main_overlap('sample_overlap')
 main_overlap('dataset_198_10')
+'''
+main_debruijn_fromstring('sample_debruijn_fromstring')
+main_debruijn_fromstring('dataset_199_6')
