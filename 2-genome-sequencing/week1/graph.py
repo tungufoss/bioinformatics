d301417e46215329f25da91af614da5fd68d4e6e
Helga Ingimundardóttir <tungufoss@gmail.com>
2016-12-14 10:07:25 +0000

    Solve the String Spelled by a Genome Path Problem
    Code Challenge: Solve the String Spelled by a Genome Path Problem.
    
    Visit the code-graded problem
    
    Extra Dataset
    
    Sample Input:
    ACCGA
    CCGAA
    CGAAG
    GAAGC
    AAGCT
    Sample Output:
    ACCGAAGCT

--- 2-genome-sequencing/week1/graph.py
+++ 2-genome-sequencing/week1/graph.py
@@ -1,8 +1,24 @@
+def ReadKmers(inputFile):
+  kmers = []
+  with open(inputFile) as inFile:
+    for line in inFile:
+      line = line.strip()
+      kmers.append(line)
+  
+  k = len(kmers[0])
+  return k,kmers
+
 def Composition(k,txt):
   kmers = []
   for i in range(len(txt)-k+1):
     kmers.append(txt[i:(i+k)])
   return kmers
+
+def StringSpelledByGenomePath(kmers):
+  txt = kmers[0]
+  for i in range(1,len(kmers)):
+    txt += kmers[i][-1]
+  return txt
 
 def main_composition(myfile):
   inputFile = myfile + '.txt'
@@ -14,5 +30,17 @@ def main_composition(myfile):
   with open(outputFile,'w') as outFile:
     outFile.write('\n'.join(kmers))
 
+def main_genomepath(myfile):
+  inputFile = myfile + '.txt'
+  outputFile = myfile + '.out'
+  k,kmers = ReadKmers(inputFile)
+  txt = StringSpelledByGenomePath(kmers)
+  with open(outputFile,'w') as outFile:
+    outFile.write(txt)
+
+'''
 main_composition('sample_composition')
 main_composition('dataset_197_3')
+'''
+main_genomepath('sample_genomepath')
+main_genomepath('dataset_198_3')
