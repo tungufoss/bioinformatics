670347861fdcd4d874471c69cfc1192e80d7c69b
Helga Ingimundardóttir <tungufoss@gmail.com>
2016-12-23 14:52:29 +0000

    [WIP] Implement LeaderboardCyclopeptideSequencing
    The tricky aspect of implementing LeaderboardCyclopeptideSequencing is
    making sure that the Trim function is implemented correctly. Check out
    Charging Station: Trimming the Peptide Leaderboard for some help with
    trimming Leaderboard.
    
    Code Challenge: Implement LeaderboardCyclopeptideSequencing.
    Input: An integer N and a collection of integers Spectrum.
    Output: LeaderPeptide after running
    LeaderboardCyclopeptideSequencing(Spectrum, N).
    
    Extra Dataset
    
    Note: Multiple solutions may exist. You may return any one.
    
    Sample Input:
    10
    0 71 113 129 147 200 218 260 313 331 347 389 460
    Sample Output:
    113-147-71-129

--- 2-genome-sequencing/week4/antibiotics.py
+++ 2-genome-sequencing/week4/antibiotics.py
@@ -5,6 +5,7 @@ sys.path.insert(0,'../week3')
 from protein import SubpeptidesCyclic, SubpeptidesNotCyclic
 
 def CyclopeptideScoring(peptide,spectrum,im_tbl,Cyclic):
+  spectrum=spectrum[:]
   if Cyclic: 
     subpeptides = SubpeptidesCyclic(peptide)
   else :
@@ -19,7 +20,42 @@ def CyclopeptideScoring(peptide,spectrum,im_tbl,Cyclic):
     
   print '{} has score {} for spectrum'.format(peptide,score)
   return score
- 
+  
+def LeaderboardExpand(leaderboard,aminos):
+  expand = []
+  for peptide in leaderboard:
+    for a in aminos :
+      expand.append(peptide+a)
+  return expand
+
+def LeaderboardTrim(leaderboard, spectrum, N):
+  return [x for x in leaderboard if len(x) <= N]
+  
+def LeaderboardCyclopeptideSequencing(spectrum,N,im_tbl):  
+  Cyclic=False
+  leader_peptide = ''
+  leaderboard = [leader_peptide]
+  leader_score = CyclopeptideScoring(leader_peptide, spectrum, im_tbl, Cyclic)
+  parentmass = spectrum[-1]  
+  aminos, aminos_tbl = aminoacids_tbl(im_tbl,True)
+  aminos = [a for a in aminos if aminos_tbl[a][2] in spectrum]
+    
+  while len(leaderboard)>0:    
+    leaderboard = LeaderboardExpand(leaderboard,aminos)
+    print len(leaderboard)
+    for peptide in leaderboard:
+      mass = PeptideMass(peptide,im_tbl)
+      if mass == parentmass :
+        score = CyclopeptideScoring(peptide, spectrum, im_tbl, Cyclic)
+        if score > leader_score:
+          leader_peptide = peptide
+          leader_score = score
+          print 'leader',peptide
+      elif mass > parentmass :
+        leaderboard.remove(peptide)        
+    leaderboard = LeaderboardTrim(leaderboard,spectrum,N)
+  return [aminos_tbl[a][2] for a in leader_peptide]
+  
 def main_CyclopeptideScoringProblem(myfile,Cyclic):
   inputFile = myfile + '.txt'
   outputFile = myfile + '.out'
@@ -32,6 +68,24 @@ def main_CyclopeptideScoringProblem(myfile,Cyclic):
   with open(outputFile,'w') as outFile:    
     outFile.write(str(score))
   
+def main_LeaderboardCyclopeptideSequencing(myfile):
+  inputFile = myfile + '.txt'
+  outputFile = myfile + '.out'
+  with open(inputFile) as inFile:
+    N = int(inFile.readline().strip())
+    spectrum = [int(x) for x in inFile.readline().strip().split(' ')]
+  
+  im_tbl = Integer_mass_table()
+  leader_peptide=LeaderboardCyclopeptideSequencing(spectrum,N,im_tbl)
+  with open(outputFile,'w') as outFile:    
+    txt='-'.join([str(p) for p in leader_peptide])
+    outFile.write(txt)
+    print txt
+  
+'''
 main_CyclopeptideScoringProblem('sample_cyclopeptidescoring',True) #11
 main_CyclopeptideScoringProblem('cyclopeptide_scoring',True) #521
 main_CyclopeptideScoringProblem('dataset_102_3',True)
+'''
+main_LeaderboardCyclopeptideSequencing('sample_linearpeptidescoring')
+#main_LeaderboardCyclopeptideSequencing('dataset_102_8')
