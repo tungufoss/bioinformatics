c0c4d87591cea8af0dc48b67d0e56faa6f193e11
Helga Ingimundardóttir <tungufoss@gmail.com>
2017-01-17 16:24:14 +0000

    Implement TrieMatching to solve the Multiple Pattern Matching Problem
    Code Challenge: Implement TrieMatching to solve the Multiple Pattern
    Matching Problem.
    Input: A string Text and a collection of strings Patterns.
    Output: All starting positions in Text where a string from Patterns
    appears as a substring.

--- 6-dna-mutations/week1/trie.py
+++ 6-dna-mutations/week1/trie.py
@@ -14,6 +14,18 @@ def TrieConstructionProblem(patterns):
 
   return edges
 
+def TrieMatching(text,patterns):
+  N = len(text)
+  indices = []
+  for pattern in patterns:
+    K = len(pattern)    
+    for i in range(0,N-K+1):
+      #print ' '*i+text[i:(i+K)]
+      if text[i:(i+K)] == pattern:
+        indices.append(i)
+  
+  return indices
+  
 def main_TrieConstructionProblem(myfile):  
   inputFile = myfile + '.txt'
   outputFile = myfile + '.out'
@@ -25,9 +37,26 @@ def main_TrieConstructionProblem(myfile):
   
   with open(outputFile, 'w') as outFile:
     for node in sorted(edges):
-      line = '{}->{}:{}'.format(node[0],edges[node],node[1])
-      print line
+      line = '{}->{}:{}'.format(node[0],edges[node],node[1])      
       outFile.write(line+'\n')
 
+def main_TrieMatchingProblem(myfile):      
+  inputFile = myfile + '.txt'
+  outputFile = myfile + '.out'
+  
+  with open(inputFile) as inFile:    
+    text = inFile.readline().strip()
+    patterns = [line.strip() for line in inFile]
+
+  indices = TrieMatching(text,patterns)
+    
+  with open(outputFile, 'w') as outFile:
+    txt = ' '.join([str(x) for x in sorted(indices)])    
+    outFile.write(txt)
+  
+'''
 main_TrieConstructionProblem('sample_TrieConstructionProblem')
 main_TrieConstructionProblem('dataset_294_4')
+''' 
+main_TrieMatchingProblem('sample_TrieMatchingProblem')
+main_TrieMatchingProblem('dataset_294_8')
