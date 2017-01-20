a4694208f0313150df23a7d88648b0b65eedea08
Helga Ingimundardóttir <tungufoss@gmail.com>
2017-01-20 11:28:40 +0000

    [FIX] Implement MaximalNonBranchingPaths.
    Used the pseudo code provided in the literature :+1:
    
    A node v in a directed graph Graph is called a 1-in-1-out node if its
    indegree and outdegree are both equal to 1, i.e., in(v) = out(v) = 1.
    We can rephrase the definition of a "maximal non-branching path" from
    the main text as a path whose internal nodes are 1-in-1-out nodes and
    whose initial and final nodes are not 1-in-1-out nodes.  Also, note that
    the definition from the main text does not handle the special case when
    Graph has a connected component that is an isolated cycle, in which all
    nodes are 1-in-1-out nodes.
    
    The MaximalNonBranchingPaths pseudocode below generates all
    non-branching paths in a graph. It iterates through all nodes of the
    graph that are not 1-in-1-out nodes and generates all non-branching
    paths starting at each such node. In a final step,
    MaximalNonBranchingPaths finds all isolated cycles in the graph.
    
    '''
    MaximalNonBranchingPaths(Graph)
    Paths ← empty list
    for each node v in Graph
    if v is not a 1-in-1-out node
    if out(v) > 0
    for each outgoing edge (v, w) from v
    NonBranchingPath ← the path consisting of the single edge (v, w)
    while w is a 1-in-1-out node
    extend NonBranchingPath by the outgoing edge (w, u) from w
    w ← u
    add NonBranchingPath to the set Paths
    for each isolated cycle Cycle in Graph
    add Cycle to Paths
    return Paths
    '''

--- 6-dna-mutations/week1/trie.py
+++ 6-dna-mutations/week1/trie.py
@@ -121,32 +121,66 @@ def AdjencyStr2List(adjacency_str):
     adjacency_list[int(from_node)] = [int(x) for x in to_nodes.split(',')]
   return adjacency_list
 
-def InOrderMaximal(from_node, start_node, connected, path_list):  
-  if from_node not in connected:
-    return [from_node] 
-    
-  children = connected[from_node]
-  del connected[from_node]   
-  for to_node in children:   
-    paths = InOrderMaximal(to_node, start_node, connected, path_list)    
-    cyclic = from_node in paths    
-    paths.append(from_node)
-    
-    # commit currently found non branching paths 
-    if len(children)>1 or from_node == start_node:      
-      path_list.append((reversed(paths),cyclic))
-      # restart fresh 
-      paths=[from_node]
-  return paths
+def Degree(adjacency_list):
+  indegree = {}
+  outdegree = {}
+  for from_node in adjacency_list:
+    outdegree[from_node] = len(adjacency_list[from_node])
+    for to_node in adjacency_list[from_node]:
+      if to_node not in indegree:
+        indegree[to_node] = 1
+      else :
+        indegree[to_node] += 1
+  for node in [n for n in indegree if n not in outdegree]:
+    outdegree[node] = 0
+  for node in [n for n in outdegree if n not in indegree]:
+    indegree[node] = 0
+  return indegree, outdegree
   
 def MaximalNonBranchingPaths(adjacency_list):
-  connected=adjacency_list.copy()  
-  path_list=[]
-  print connected  
-  while len(connected)>0:    
-    root = min(connected)
-    InOrderMaximal(root, root, connected, path_list)  
-  return path_list
+  import copy
+  unused = copy.deepcopy(adjacency_list)  
+  
+  paths =[] # Paths <- empty list
+  indegree, outdegree = Degree(adjacency_list)
+  
+  # for each node v in Graph
+  for v in adjacency_list : 
+    # if v is not a 1-in-1-out node
+    if indegree[v] != 1 or outdegree[v] != 1 : 
+      if outdegree[v]>0 :
+        # for each outgoing edge (v, w) from v
+        for w in adjacency_list[v] :
+          # NonBranchingPath <- the path consisting of the single edge (v, w)
+          nonBranchingPath = [v,w]
+          unused[v].remove(w)
+          # while w is a 1-in-1-out node
+          while indegree[w] == 1 and outdegree[w] == 1 :
+            u = adjacency_list[w][0]
+            #extend NonBranchingPath by the outgoing edge (w, u) from w 
+            nonBranchingPath.append(u)
+            unused[w].remove(u)
+            w = u # w <- u
+          # add NonBranchingPath to the set Paths
+          paths.append(nonBranchingPath)
+  
+  #for each isolated cycle Cycle in Graph  
+  for v in adjacency_list :      
+    if unused[v] == [] : continue
+  
+    w = adjacency_list[v][0]
+    cycle = [v,w]
+    unused[v].remove(w)
+    
+    # add Cycle to Paths    
+    while v != w :      
+      u = adjacency_list[w][0]
+      cycle.append(u)
+      unused[w].remove(u)
+      w = u      
+    paths.append(cycle)
+  
+  return paths
   
 def main_TrieConstructionProblem(myfile):  
   inputFile = myfile + '.txt'
@@ -237,15 +271,13 @@ def main_MaximalNonBranchingPaths(myfile):
   with open(inputFile) as inFile:        
     adjacency_str = [line.strip() for line in inFile]
 
-  adjacency_list = AdjencyStr2List(adjacency_str)
+  adjacency_list = AdjencyStr2List(adjacency_str)  
   paths = MaximalNonBranchingPaths(adjacency_list)
-  print paths 
+
   with open(outputFile, 'w') as outFile:
-    for cyclic in [False, True]: 
-      for path in sorted([p[0] for p in paths if p[1]==cyclic]):
-        txt = ' -> '.join([str(x) for x in path])
-        outFile.write(txt+'\n')
-        print txt
+    for path in paths :
+      txt = ' -> '.join([str(x) for x in path])
+      outFile.write(txt+'\n')        
     
 '''
 main_TrieConstructionProblem('sample_TrieConstructionProblem')
@@ -266,4 +298,5 @@ main_ShortestNonSharedSubstringProblem('ShortestNonSharedSubstring')
 main_ShortestNonSharedSubstringProblem('dataset_296_7')
 '''
 main_MaximalNonBranchingPaths('sample_MaximalNonBranchingPaths')
+main_MaximalNonBranchingPaths('MaximalNonBranchingPaths')
 main_MaximalNonBranchingPaths('dataset_6207_2')
