45b355c127f450aa8a3f9473934c42f8ea281b1b
Helga Ingimundardóttir <tungufoss@gmail.com>
2017-01-20 16:34:20 +0000

    Solve the Burrows-Wheeler Transform Construction Problem
    Burrows-Wheeler Transform Construction Problem: Construct the
    Burrows-Wheeler transform of a string.
    Input: A string Text.
    Output: BWT(Text).
    
    Code Challenge: Solve the Burrows-Wheeler Transform Construction
    Problem.
    
    Note: Although it is possible to construct the Burrows-Wheeler transform
    in O(|Text|) time and space, we do not expect you to implement such a
    fast algorithm. In other words, it is perfectly fine to produce
    BWT(Text) by first producing the complete Burrows-Wheeler matrix
    M(Text).
    
    Sample Input:
    GCGTGCCTGGTCA$
    Sample Output:
    ACTGGCT$TGCGGC

--- 6-dna-mutations/week2/BurrowsWheelerTransform.py
+++ 6-dna-mutations/week2/BurrowsWheelerTransform.py
@@ -8,10 +8,21 @@ def SuffixArrayConstruction(text):
   for s in sorted(suffixes):
     starting_pos.append(N-len(s))
   return starting_pos
-  
-  for s in suffixes:
-    print s 
-  return []
+
+def BurrowsWheelerTransformConstruction(text):  
+  N=len(text)
+  cyclic_rotations=[]  
+  # First, form all possible cyclic rotations of Text;   
+  for i in range(N):
+    cyclic_rotations.append(text)
+    # a cyclic rotation is defined by chopping off a suffix from the end of Text and appending this suffix to the beginning of Text. 
+    text = text[-1]+text[:-1]
+  print text
+  # Next - similarly to suffix arrays - order all the cyclic rotations of Text lexicographically to form a |Text| x |Text| matrix of symbols that we call the Burrows-Wheeler matrix and denote by M(Text). 
+  M = sorted(cyclic_rotations)  
+  # We are interested in the last column of M(Text), called the Burrows-Wheeler transform of Text, or BWT(Text)
+  BWT = ''.join([m[-1] for m in M])  
+  return BWT
 
 def main_SuffixArrayConstruction(myfile):
   inputFile = myfile + '.txt'
@@ -26,6 +37,23 @@ def main_SuffixArrayConstruction(myfile):
     txt = ', '.join([str(x) for x in suffix_arr])
     outFile.write(txt)  
     print txt
+
+def main_BurrowsWheelerTransformConstruction(myfile):
+  inputFile = myfile + '.txt'
+  outputFile = myfile + '.out'
   
+  with open(inputFile) as inFile:        
+    text = inFile.readline().strip()
+  
+  BWT = BurrowsWheelerTransformConstruction(text)
+  
+  with open(outputFile, 'w') as outFile:        
+    outFile.write(BWT)  
+    print BWT  
+    
+'''  
 main_SuffixArrayConstruction('sample_SuffixArrayConstruction')
 main_SuffixArrayConstruction('dataset_310_2')
+'''
+main_BurrowsWheelerTransformConstruction('sample_BurrowsWheelerTransformConstruction')
+main_BurrowsWheelerTransformConstruction('dataset_297_5')
