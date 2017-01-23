62d0068d6c4df5b8954524c9ca3e18582cd4ca5b
Helga Ingimundardóttir <tungufoss@gmail.com>
2017-01-23 11:19:43 +0000

    [WIP] Inverse Burrows-Wheeler Transform Problem
    Inverse Burrows-Wheeler Transform Problem: Reconstruct a string from its
    Burrows-Wheeler transform.
    Input: A string Transform (with a single "$$" symbol).
    Output: The string Text such that BWT(Text) = Transform.
    
    Code Challenge: Solve the Inverse Burrows-Wheeler Transform Problem.
    
    Sample Input:
    TTCCTAACG$A
    Sample Output:
    TACATCACGT$
    
    Works for sample and exercise break datasets, but not for submisson set
    dataset_299_10.txt :-1:

--- 6-dna-mutations/week2/BurrowsWheelerTransform.py
+++ 6-dna-mutations/week2/BurrowsWheelerTransform.py
@@ -24,6 +24,34 @@ def BurrowsWheelerTransformConstruction(text):
   BWT = ''.join([m[-1] for m in M])  
   return BWT
 
+def InvBurrowsWheelerTransformConstruction(transform):
+  transform = list(transform)
+  N = len(transform)
+  
+  # add subscripts
+  for i in reversed(range(N)):    
+    transform[i] += str(transform[:i+1].count(transform[i]))
+  
+  FirstColumn = sorted(transform)
+  LastColumn = transform    
+  
+  M = {}
+  for i in range(N):
+    M[i] = [FirstColumn[i]]+['?']*(N-2)+[LastColumn[i]]
+  
+  # Using the First-Last Property for inverting the Burrows-Wheeler transform    
+  for row in range(N):    
+    for col in range(1,N-1):    
+      char = M[row][col-1]     
+      nrow = LastColumn.index(char) 
+      M[row][col] = M[nrow][0] # First column
+    
+  # for m in sorted(M): print m, [x[0] for x in M[m] ] # remove subscripts
+  
+  row = LastColumn.index('$1')
+  text= ''.join([ M[row][i][0] for i in range(N) ])  
+  return text
+  
 def main_SuffixArrayConstruction(myfile):
   inputFile = myfile + '.txt'
   outputFile = myfile + '.out'
@@ -50,10 +78,28 @@ def main_BurrowsWheelerTransformConstruction(myfile):
   with open(outputFile, 'w') as outFile:        
     outFile.write(BWT)  
     print BWT  
+
+def main_InverseBurrowsWheelerTransform(myfile):
+  inputFile = myfile + '.txt'
+  outputFile = myfile + '.out'
+  
+  with open(inputFile) as inFile:        
+    transform = inFile.readline().strip()
     
+  text = InvBurrowsWheelerTransformConstruction(transform)
+  print 'Sanity check: {}'.format(transform == BurrowsWheelerTransformConstruction(text))
+  
+  with open(outputFile, 'w') as outFile:        
+    outFile.write(text)  
+    print text
+
 '''  
 main_SuffixArrayConstruction('sample_SuffixArrayConstruction')
 main_SuffixArrayConstruction('dataset_310_2')
-'''
 main_BurrowsWheelerTransformConstruction('sample_BurrowsWheelerTransformConstruction')
 main_BurrowsWheelerTransformConstruction('dataset_297_5')
+'''
+main_InverseBurrowsWheelerTransform('sample_InverseBurrowsWheelerTransformConstruction')
+main_InverseBurrowsWheelerTransform('exbreak_InverseBurrowsWheelerTransformConstruction')
+main_InverseBurrowsWheelerTransform('dataset_299_10')
+
