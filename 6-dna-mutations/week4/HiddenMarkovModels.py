12084d9a49ee4f2b852f790c609f04b8e948e8cc
Helga Ingimundardóttir <tungufoss@gmail.com>
2017-01-26 17:12:54 +0000

    Solve the Probability of an Outcome Given a Hidden Path Problem
    CODE CHALLENGE: Solve the Probability of an Outcome Given a Hidden Path
    Problem.
    Input: A string x, followed by the alphabet from which x was
    constructed, followed by
    a hidden path π, followed by the states States and emission matrix
    Emission of an HMM
    (Σ, States, Transition, Emission).
    Output: The conditional probability Pr(x|π) that x will be emitted given
    that the HMM
    follows the hidden path π.
    
    Note: You may assume that transitions from the initial state occur with
    equal probability.
    
    Sample Input:
    zzzyxyyzzx
    --------
    x y z
    --------
    BAAAAAAAAA
    --------
    A B
    --------
    x y z
    A 0.176 0.596 0.228
    B 0.225 0.572 0.203
    Sample Output:
    3.59748954746e-06

--- 6-dna-mutations/week4/HiddenMarkovModels.py
+++ 6-dna-mutations/week4/HiddenMarkovModels.py
@@ -4,6 +4,23 @@ def ProbabilityHiddenPathProblem(Sigma, States, Transition):
     p*=Transition[(Sigma[i-1],Sigma[i])]  
   return p
 
+def ProbabilityOutcomeGivenHPP(x, x_states, pi, pi_states, Emmision):
+  p = 1  
+  for i in range(len(x)):
+    p*=Emmision[(pi[i],x[i])]
+  
+  return p
+  
+def ReadMatrix(inFile):
+  colnames = inFile.readline().strip().split('\t')
+  nCol = len(colnames)
+  mat = {}
+  for line in inFile:
+    line = line.strip().split('\t')
+    for i in range(nCol):
+      mat[(line[0],colnames[i])]=float(line[i+1])
+  return mat
+  
 def main_ProbabilityHiddenPathProblem(myfile):
   inputFile = myfile + '.txt'
   outputFile = myfile + '.out'
@@ -13,18 +30,38 @@ def main_ProbabilityHiddenPathProblem(myfile):
     inFile.readline() # --------
     States = inFile.readline().strip().split(' ')
     inFile.readline() # --------
-    colnames = inFile.readline().strip().split('\t')
-    nCol = len(colnames)
-    Transition = {}
-    for line in inFile:
-      line = line.strip().split('\t')
-      for i in range(nCol):
-        Transition[(line[0],colnames[i])]=float(line[i+1])
+    Transition = ReadTransition(inFile)
   
   prob = ProbabilityHiddenPathProblem(Sigma, States, Transition)
   with open(outputFile, 'w') as outFile:    
     outFile.write(str(prob))
     print 'Probability of a Hidden Path Problem for {}: {}'.format(Sigma, prob)
+    
+def main_ProbabilityOutcomeGivenHPP(myfile):
 
+  inputFile = myfile + '.txt'
+  outputFile = myfile + '.out'
+  
+  with open(inputFile) as inFile:        
+    x = inFile.readline().strip()
+    inFile.readline() # --------
+    x_states = inFile.readline().strip().split(' ')
+    inFile.readline() # --------
+    pi = inFile.readline().strip()
+    inFile.readline() # --------
+    pi_states = inFile.readline().strip().split(' ')
+    inFile.readline() # --------
+    Emmision = ReadMatrix(inFile)
+
+  prob = ProbabilityOutcomeGivenHPP(x, x_states, pi, pi_states, Emmision)
+  with open(outputFile, 'w') as outFile:    
+    outFile.write(str(prob))
+    print 'Probability that an HMM will emit a {} given its hidden path {}: {}'.format(x, pi, prob)
+
+'''
 main_ProbabilityHiddenPathProblem('sample_ProbabilityHiddenPathProblem')
 main_ProbabilityHiddenPathProblem('dataset_11594_2')
+'''
+main_ProbabilityOutcomeGivenHPP('sample_ProbabilityOutcomeGivenHPP')
+main_ProbabilityOutcomeGivenHPP('extra_ProbabilityOutcomeGivenHPP')
+main_ProbabilityOutcomeGivenHPP('dataset_11594_4')
