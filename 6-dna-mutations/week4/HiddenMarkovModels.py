e2e90ad2eacbb017217f115ec790c290dcc63e82
Helga Ingimundardóttir <tungufoss@gmail.com>
2017-01-27 17:06:31 +0000

    [WIP] Implement the Viterbi algorithm solving the Decoding Problem
    CODE CHALLENGE: Implement the Viterbi algorithm solving the Decoding
    Problem.
    Input: A string x, followed by the alphabet from which x was
    constructed,
    followed by the states States, transition matrix Transition, and
    emission matrix
    Emission of an HMM (Σ, States, Transition, Emission).
    Output: A path that maximizes the (unconditional) probability Pr(x, π)
    over all possible paths π.
    
    Note: You may assume that transitions from the initial state occur with
    equal probability.
    
    Sample Input:
    xyxzzxyxyy
    --------
    x y z
    --------
    A B
    --------
    A B
    A 0.641 0.359
    B 0.729 0.271
    --------
    x y z
    A 0.117 0.691 0.192
    B 0.097 0.42 0.483
    Sample Output:
    AAABBAAAAA
    
    Current version of the algorithm does not yield the correct output for
    th sample dataset :-1:

--- 6-dna-mutations/week4/HiddenMarkovModels.py
+++ 6-dna-mutations/week4/HiddenMarkovModels.py
@@ -10,17 +10,35 @@ def ProbabilityOutcomeGivenHPP(x, x_states, pi, pi_states, Emmision):
     p*=Emmision[(pi[i],x[i])]
   
   return p
+
+def ViterbiAlgorithm(x, x_states, pi_states, Transition, Emmision):  
+  # https://www.coursera.org/learn/dna-mutations/lecture/9foxL/the-viterbi-algorithm
+  prob1 = {}  
+  pi = []  
+  for i in range(len(x)) :
+    if i == 0 : 
+      for l in pi_states :
+        prob1[l] = 1 * 0.5 * Emmision[(l,x[0])]
+    else : 
+      prob0=prob1.copy()
+      for l in pi_states :
+        prob1[l] = prob0[l]*Transition[(pi[-1],l)]*Emmision[(l,x[i])]    
+    maxval = max([prob1[k] for k in pi_states])
+    maxvar = [k for k in pi_states if prob1[k] == maxval][0]    
+    pi.append(maxvar)    
+  return pi
   
-def ReadMatrix(inFile):
+def ReadMatrix(inFile, nRows):
   colnames = inFile.readline().strip().split('\t')
   nCol = len(colnames)
   mat = {}
-  for line in inFile:
-    line = line.strip().split('\t')
+  for ii in range(nRows):       
+    line = inFile.readline()    
+    line = line.strip().split('\t')    
     for i in range(nCol):
       mat[(line[0],colnames[i])]=float(line[i+1])
   return mat
-  
+
 def main_ProbabilityHiddenPathProblem(myfile):
   inputFile = myfile + '.txt'
   outputFile = myfile + '.out'
@@ -30,7 +48,7 @@ def main_ProbabilityHiddenPathProblem(myfile):
     inFile.readline() # --------
     States = inFile.readline().strip().split(' ')
     inFile.readline() # --------
-    Transition = ReadTransition(inFile)
+    Transition = ReadMatrix(inFile, len(States))
   
   prob = ProbabilityHiddenPathProblem(Sigma, States, Transition)
   with open(outputFile, 'w') as outFile:    
@@ -51,17 +69,38 @@ def main_ProbabilityOutcomeGivenHPP(myfile):
     inFile.readline() # --------
     pi_states = inFile.readline().strip().split(' ')
     inFile.readline() # --------
-    Emmision = ReadMatrix(inFile)
-
+    Emmision = ReadMatrix(inFile, len(pi_states))
+  
   prob = ProbabilityOutcomeGivenHPP(x, x_states, pi, pi_states, Emmision)
   with open(outputFile, 'w') as outFile:    
     outFile.write(str(prob))
     print 'Probability that an HMM will emit a {} given its hidden path {}: {}'.format(x, pi, prob)
 
+def main_ViterbiAlgorithm(myfile):    
+  inputFile = myfile + '.txt'
+  outputFile = myfile + '.out'
+  
+  with open(inputFile) as inFile:        
+    x = inFile.readline().strip()
+    inFile.readline() # --------
+    x_states = inFile.readline().strip().split(' ')
+    inFile.readline() # --------
+    pi_states = inFile.readline().strip().split(' ')
+    inFile.readline() # --------
+    Transition = ReadMatrix(inFile,len(pi_states))    
+    inFile.readline() # --------
+    Emmision = ReadMatrix(inFile,len(pi_states))
+    
+  pi = ViterbiAlgorithm(x, x_states, pi_states, Transition, Emmision)
+  HMM = ''.join(pi)
+  print pi
+    
 '''
 main_ProbabilityHiddenPathProblem('sample_ProbabilityHiddenPathProblem')
 main_ProbabilityHiddenPathProblem('dataset_11594_2')
-'''
 main_ProbabilityOutcomeGivenHPP('sample_ProbabilityOutcomeGivenHPP')
 main_ProbabilityOutcomeGivenHPP('extra_ProbabilityOutcomeGivenHPP')
 main_ProbabilityOutcomeGivenHPP('dataset_11594_4')
+'''
+#main_ViterbiAlgorithm('sample_ViterbiAlgorithm')
+main_ViterbiAlgorithm('sample_crookedcasino')
